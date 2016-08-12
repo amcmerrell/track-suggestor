24b3b045897adbe27474fd060af91546138e90c3
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-08-12 12:22:59 -0700

    add branching tied to questions 3 and 4.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,40 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     var nameInput = $("input#name").val();
     var devInput = $("input:radio[name=devType]:checked").val();
+    var bizInput = $("input:radio[name=bizSize]:checked").val();
+    var osInput = $("#osPref").val();
+    var buildInput = $("input:radio[name=buildType]:checked").val();
 
     // $(".recommendation").text(nameInput + ", here are our course recommendations for you!");
 
     if (devInput === "frontend") {
       $("#css").show();
+      $("#java").show();
+      $("#csharp").show();
+      $("#php").hide();
+      $("#ruby").hide();
+    } else {
+      if (bizInput === "small") {
+        $("#ruby").show();
+        $("#php").show();
+        $("#css").show();
+        $("#csharp").hide();
+        $("#java").hide();
+      } else if (bizInput === "large" && osInput !== "neither") {
+        $("#csharp").show();
+        $("#java").show();
+        $("#php").show();
+        $("#css").hide();
+        $("#ruby").hide();
+      } else {
+        if (buildInput === "web" || buildInput === "cms" || buildInput === "webapp") {
+          $("#css").show();
+          $("#ruby").show();
+          $("#php").show();
+          $("#java").hide();
+          $("#csharp").hide();
+        }
+      }
     }
 
     event.preventDefault();
