25ff436b34a04812d9edf23c69383895c9178024
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-08-12 13:36:42 -0700

    comment out previous js branch logic which was not working and add adjust variables to allow for number value based branch logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,50 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
-    var nameInput = $("input#name").val();
-    var devInput = $("input:radio[name=devType]:checked").val();
-    var bizInput = $("input:radio[name=bizSize]:checked").val();
-    var osInput = $("#osPref").val();
-    var buildInput = $("input:radio[name=buildType]:checked").val();
+    var nameInput = parseInt($("input#name").val());
+    var devInput = parseInt($("input:radio[name=devType]:checked").val());
+    var bizInput = parseInt($("input:radio[name=bizSize]:checked").val());
+    var osInput = parseInt($("#osPref").val());
+    var buildInput = parseInt($("input:radio[name=buildType]:checked").val());
+    var result = devInput + bizInput + osInput + buildInput;
 
     // $(".recommendation").text(nameInput + ", here are our course recommendations for you!");
 
-    if (devInput === "frontend") {
-      $("#css").show();
-      $("#java").show();
-      $("#csharp").show();
-      $("#php").hide();
-      $("#ruby").hide();
-    } else {
-      if (bizInput === "small") {
-        $("#ruby").show();
-        $("#php").show();
-        $("#css").show();
-        $("#csharp").hide();
-        $("#java").hide();
-      } else if (bizInput === "large" && osInput !== "neither") {
-        $("#csharp").show();
-        $("#java").show();
-        $("#php").show();
-        $("#css").hide();
-        $("#ruby").hide();
-      } else {
-        if (buildInput === "web" || buildInput === "cms" || buildInput === "webapp") {
-          $("#css").show();
-          $("#ruby").show();
-          $("#php").show();
-          $("#java").hide();
-          $("#csharp").hide();
-        }
-      }
-    }
+    if
 
     event.preventDefault();
   });
 });
+
+    // if (devInput === "frontend") {
+    //   $("#css").show();
+    //   $("#java").show();
+    //   $("#csharp").show();
+    //   $("#php").hide();
+    //   $("#ruby").hide();
+    // } else {
+    //   if (bizInput === "small") {
+    //     $("#ruby").show();
+    //     $("#php").show();
+    //     $("#css").show();
+    //     $("#csharp").hide();
+    //     $("#java").hide();
+    //   } else if (bizInput === "large") {
+    //     $("#csharp").show();
+    //     $("#java").show();
+    //     $("#php").show();
+    //     $("#css").hide();
+    //     $("#ruby").hide();
+    //   } else {
+    //     if (buildInput === "web" || buildInput === "cms" || buildInput === "webapp") {
+    //       $("#css").show();
+    //       $("#ruby").show();
+    //       $("#php").show();
+    //       $("#java").hide();
+    //       $("#csharp").hide();
+    //     } else {
+    //         $("#java").show();
+    //         $("#csharp").show();
+    //         $("#ruby").show();
+    //         $("#css").hide();
+    //         $("#php").hide();
+    //       };
